63b5c902bd664b26e3a608effa31d3d19b24428a
userimack <mahendra.k12@gmail.com>
2016-01-26 03:13:16 +0530

    first script

---
+++ selenium_script.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, ElementNotVisibleException
+
+def init_driver():
+    driver = webdriver.Firefox()
+    driver.wait = WebDriverWait(driver, 5)
+    return driver
+
+
+def lookup(driver, query):
+    driver.get("http://www.google.com")
+    try:
+        box = driver.wait.until(EC.presence_of_element_located((By.NAME, "q")))
+        button = driver.wait.until(EC.element_to_be_clickable((By.NAME, "btnK")))
+        box.send_keys(query)
+        try:
+            button.click()
+        except ElementNotVisibleException:
+            #button = driver.wait.until(EC.visiblity_of_element_located((By.NAME, "btnG")))
+            button = driver.wait.until(EC.element_to_be_clickable((By.NAME, "btnG")))
+            button.click()
+    except TimeoutException:
+        print("Box or Button not found in googl.com")
+
+
+if __name__ == "__main__":
+    driver = init_driver()
+    lookup(driver, "userimack")
+    print ("Good Bye..... Quitting")
+    time.sleep(10)
+    driver.quit()
